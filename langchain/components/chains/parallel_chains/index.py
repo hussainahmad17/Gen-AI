0235354eee88ef43306d77049a773f689ca76174
Hussain Ahmad <hussainahmad.dev.17@gmail.com>
2026-07-09 14:39:21 +0500

    add missing keys

--- langchain/components/chains/parallel_chains/index.py
+++ langchain/components/chains/parallel_chains/index.py
@@ -2,6 +2,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
