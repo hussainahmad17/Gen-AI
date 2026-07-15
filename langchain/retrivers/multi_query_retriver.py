bedf095375056f82f7a002eb85c053a1371ea639
Hussain Ahmad <hussainahmad.dev.17@gmail.com>
2026-07-15 18:58:30 +0500

    updating docs and implementing multi-query retriver

---
+++ langchain/retrivers/multi_query_retriver.py
@@ -0,0 +1,38 @@
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
+from langchain.retrievers.multi_query_retriever import MultiQueryRetriever
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+
+
+
+docs = [
+    Document(
+       page_content="Virat Kohli is one of the most successful and consistent batsmen in IPL history. Known for his aggressive batting style and exceptional run-scoring ability, he has been a key player for Royal Challengers Bangalore."
+    ),
+    Document(
+        page_content="Rohit Sharma is the most successful captain in IPL history, leading Mumbai Indians to five titles. He's known for his calm leadership and elegant batting style."
+    ),
+    Document(
+        page_content="MS Dhoni, famously known as Captain Cool, has led Chennai Super Kings to multiple IPL titles. His finishing skills, wicket-keeping, and tactical captaincy are legendary."
+    ),
+    Document(
+        page_content="Jasprit Bumrah is considered one of the best fast bowlers in T20 cricket. Playing for Mumbai Indians, he is known for his yorkers and death-over bowling."
+    ),
+]
+
+vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+
+
+multi_query_retriever = MultiQueryRetriever.from_llm(
+    retriever=vector_store.as_retriever(search_kwargs={"k": 3}),
+    llm=ChatOpenAI(model_name="gpt-3.5-turbo")
+)
+
+query = "Who is the most famous among them?"
+results = multi_query_retriever.invoke(query)
+for i in results:
+    print("Most similar document:")
+    print(i.page_content)
